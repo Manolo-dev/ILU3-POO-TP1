a384ea9337884796d071587eb2fa4c16597b2b1a
Manolo-dev <sardo.manolo.dev@gmail.com>
2025-02-13 09:20:54 +0100

    Ajout toString et correction Type

--- cartes/Probleme.java
+++ cartes/Probleme.java
@@ -3,12 +3,17 @@ package cartes;
 public abstract class Probleme extends Carte {
 	private Type type;
 	
-	public Probleme(Type type) {
+	protected Probleme(Type type) {
 		this.type = type;
 	}
 	
 	public Type getType() {
 		return type;
+	}
+
+	@Override
+	public String toString() {
+		return getType().getNom(this);
 	}
 
 }
--- cartes/Type.java
+++ cartes/Type.java
@@ -1,8 +1,31 @@
 package cartes;
 
 public enum Type {
-    FEU,
-    ESSENCE,
-    CREVAISON,
-    ACCIDENT
+    FEU("FeuRouge", "FeuVert", "VehiculePrioritaire"),
+    ESSENCE("PanneEssence", "Essence", "Citerne"),
+    CREVAISON("PanneCrevaison", "Crevaison", "RoueDeSecours"),
+    ACCIDENT("Accident", "Reparation", "Asurance");
+
+    private final String nomAttaque;
+    private final String nomParade;
+    private final String nomBotte;
+
+    Type(String nomAttaque, String nomParade, String nomBotte) {
+        this.nomAttaque = nomAttaque;
+        this.nomParade = nomParade;
+        this.nomBotte = nomBotte;
+    }
+    
+    public String getNom(Probleme probleme) {
+        if(probleme instanceof Attaque)
+            return nomAttaque;
+        
+        if(probleme instanceof Parade)
+            return nomParade;
+        
+        if(probleme instanceof Botte)
+            return nomBotte;
+        
+        return null;
+    }
 }
